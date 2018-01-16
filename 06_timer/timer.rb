4de8ce94081ebae5eea8c678c17f0bfb4732f255
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-16 23:58:10 +0100

    debut exo 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  attr_reader :seconds
+  attr_writer :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    time = @seconds 
+  end
 end
