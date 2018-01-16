2fc7a69283901db82b62010cada2ba454a0f4c9b
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-16 23:18:19 +0100

    exo5 ok

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,3 +10,24 @@ end
 def sum (array)
   return array.inject(0){|sum,x| sum + x }
 end
+
+def multiply(a,b)
+  a*b
+
+end
+
+def power(number, pw)
+  res = 1
+  pw.times do
+    res=res*number
+  end
+  res
+end
+
+def factorial(nmb)
+  res = 1
+  if nmbr == 0 then 1 end
+  3.times do |i|
+    res = res*(i+1)
+  end
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  attr_reader :title
+  attr_writer :title
+
+  def title()
+     prepo_conjunc_article =["and", "a","an","the", "for","of","by","over","at","on" , "in" ,
+        "of" , "for" , "from" , "to" , "into" , "by" , "across" , "around" , "near" , "with" , "about" , "along"]
+     #si l'element n'est pas dans la liste des préposition+conjonctions+articles, on la capitalize
+     @title = @title.split.map{|x| ( (not prepo_conjunc_article.include?(x))? x.capitalize : x) }*' '
+     @title= @title.split
+     @title[0] = @title[0].capitalize
+     @title = @title*' '
+  end
 end
