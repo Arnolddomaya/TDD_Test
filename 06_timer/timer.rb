d7001bd29b8d88d7e6a9862f0cdcda8777be8270
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-17 12:01:13 +0100

    exo06 ok

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -11,6 +11,12 @@ class Timer
   end
 
   def time_string
-    time = @seconds 
+    time, seconds,mins,hours = @seconds.to_i, 0,0,0
+    #on prend la partie entière du resultat de la division
+    seconds = time%60
+    mins = (time/60).to_i
+    hours = (mins.to_f/60).to_i
+    mins = mins.modulo(60)
+    "%02d:%02d:%02d" % [hours,mins,seconds]
   end
 end
